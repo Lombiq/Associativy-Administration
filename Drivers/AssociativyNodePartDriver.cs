9b94d3c37884486e192a578f3d9a76d5c0abd0a7
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-01-13 15:52:45 +0100

    Initial commit after separation from Associativy module

---
+++ Drivers/AssociativyNodePartDriver.cs
@@ -0,0 +1,38 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Environment.Extensions;
+using Associativy.Administration.Models;
+
+namespace Associativy.Administration.Drivers
+{
+    [OrchardFeature("Associativy")]
+    public class AssociativyNodePartDriver : ContentPartDriver<AssociativyNodePart>
+    {
+        protected override string Prefix
+        {
+            get { return "Associativy.NodePart"; }
+        }
+
+        //protected override DriverResult Display(AssociativyNodePart part, string displayType, dynamic shapeHelper)
+        //{
+        //}
+
+        //// GET
+        //protected override DriverResult Editor(AssociativyNodePart part, dynamic shapeHelper)
+        //{
+        //    //return ContentShape("Parts_SearchForm",
+        //    //    () => shapeHelper.EditorTemplate(
+        //    //            TemplateName: "Parts/SearchForm",
+        //    //            Model: part,
+        //    //            Prefix: Prefix));
+        //}
+
+        //// POST
+        //protected override DriverResult Editor(AssociativyNodePart part, IUpdateModel updater, dynamic shapeHelper)
+        //{
+        //    updater.TryUpdateModel(part, Prefix, null, null);
+
+        //    return Editor(part, shapeHelper);
+        //}
+    }
+}
---
+++ Models/AssociativyNodePart.cs
@@ -0,0 +1,11 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+
+namespace Associativy.Administration.Models
+{
+    [OrchardFeature("Associativy")]
+    // Soon needed so that connections can be added on the admin page
+    public class AssociativyNodePart : ContentPart
+    {
+    }
+}
