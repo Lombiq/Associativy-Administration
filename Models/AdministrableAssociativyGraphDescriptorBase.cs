cf72667bfd4f500aafed21a27b092e02491a210a
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-01-13 20:23:07 +0100

    AdministrableAssociativyGraphDescriptorBase

---
+++ Models/AdministrableAssociativyGraphDescriptorBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Environment.Extensions;
+using Associativy.Models;
+using Piedone.HelpfulLibraries.DependencyInjection;
+using Associativy.Services;
+
+namespace Associativy.Administration.Models
+{
+    [OrchardFeature("Associativy.Administration")]
+    public abstract class AdministrableAssociativyGraphDescriptorBase<TNodeToNodeConnectorRecord> : AssociativyGraphDescriptorBase<TNodeToNodeConnectorRecord>
+        where TNodeToNodeConnectorRecord : INodeToNodeConnectorRecord, new()
+    {
+        public override string[] ContentTypes
+        {
+            get
+            {
+                // Lazy-loading part
+                return base.ContentTypes;
+            }
+        }
+
+        public AdministrableAssociativyGraphDescriptorBase(IResolve<IConnectionManager<TNodeToNodeConnectorRecord>> connectionManagerResolver)
+            : base(connectionManagerResolver)
+        {
+        }
+    }
+}
