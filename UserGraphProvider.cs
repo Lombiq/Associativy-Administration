d7f0f71dad70f0e985c03a5561334f6a3bdb3bb3
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-04-19 11:29:39 +0200

    UserGraphProvider caching

--- UserGraphProvider.cs
+++ UserGraphProvider.cs
@@ -9,25 +9,39 @@ using Associativy.Administration.Models;
 using Piedone.HelpfulLibraries.DependencyInjection;
 using Orchard.ContentManagement;
 using Orchard.Core.Common.Fields;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Caching;
 
 namespace Associativy.Administration
 {
     [OrchardFeature("Associativy.Administration.UserGraphs")]
-    public class UserGraphProvider : GraphProviderBase<IDatabaseConnectionManager<UserGraphNodeConnector>>
+    public class UserGraphProvider : GraphProviderBase<IDatabaseConnectionManager<UserGraphNodeConnector>>, IContentHandler
     {
         private readonly IContentManager _contentManager;
+        private readonly ICacheManager _cacheManager;
+        private readonly ISignals _signals;
+
+        private const string _graphChangedSignal = "Associativy.Administration.UserGraphs.GraphsChanged";
 
         public UserGraphProvider(
             IResolve<IDatabaseConnectionManager<UserGraphNodeConnector>> connectionManagerResolver,
-            IContentManager contentManager)
+            IContentManager contentManager,
+            ICacheManager cacheManager,
+            ISignals signals)
             : base(connectionManagerResolver)
         {
             _contentManager = contentManager;
+            _cacheManager = cacheManager;
+            _signals = signals;
         }
 
         public override void Describe(DescribeContext describeContext)
         {
-            var graphs = _contentManager.Query("AssociativyGraph").List();
+            var graphs = _cacheManager.Get("Associativy.Administration.UserGraph", ctx =>
+            {
+                ctx.Monitor(_signals.When(_graphChangedSignal));
+                return _contentManager.Query("AssociativyGraph").List();
+            });
 
             foreach (var graph in graphs)
             {
@@ -38,6 +52,126 @@ namespace Associativy.Administration
                     graphPart.ContentTypes.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries),
                     ConnectionManager);
             }
+        }
+
+        public void Activating(ActivatingContentContext context)
+        {
+        }
+
+        public void Activated(ActivatedContentContext context)
+        {
+        }
+
+        public void Initializing(InitializingContentContext context)
+        {
+        }
+
+        public void Creating(CreateContentContext context)
+        {
+        }
+
+        public void Created(CreateContentContext context)
+        {
+            TriggerIfGraph(context);
+        }
+
+        public void Loading(LoadContentContext context)
+        {
+        }
+
+        public void Loaded(LoadContentContext context)
+        {
+        }
+
+        public void Updating(UpdateContentContext context)
+        {
+        }
+
+        public void Updated(UpdateContentContext context)
+        {
+            TriggerIfGraph(context);
+        }
+
+        public void Versioning(VersionContentContext context)
+        {
+        }
+
+        public void Versioned(VersionContentContext context)
+        {
+        }
+
+        public void Publishing(PublishContentContext context)
+        {
+        }
+
+        public void Published(PublishContentContext context)
+        {
+            TriggerIfGraph(context);
+        }
+
+        public void Unpublishing(PublishContentContext context)
+        {
+        }
+
+        public void Unpublished(PublishContentContext context)
+        {
+            TriggerIfGraph(context);
+        }
+
+        public void Removing(RemoveContentContext context)
+        {
+        }
+
+        public void Removed(RemoveContentContext context)
+        {
+            TriggerIfGraph(context);
+        }
+
+        public void Indexing(IndexContentContext context)
+        {
+        }
+
+        public void Indexed(IndexContentContext context)
+        {
+        }
+
+        public void Importing(ImportContentContext context)
+        {
+        }
+
+        public void Imported(ImportContentContext context)
+        {
+            TriggerIfGraph(context);
+        }
+
+        public void Exporting(ExportContentContext context)
+        {
+        }
+
+        public void Exported(ExportContentContext context)
+        {
+        }
+
+        public void GetContentItemMetadata(GetContentItemMetadataContext context)
+        {
+        }
+
+        public void BuildDisplay(BuildDisplayContext context)
+        {
+        }
+
+        public void BuildEditor(BuildEditorContext context)
+        {
+        }
+
+        public void UpdateEditor(UpdateEditorContext context)
+        {
+        }
+
+        private void TriggerIfGraph(ContentContextBase context)
+        {
+            if (context.ContentType != "AssociativyGraph") return;
+            _signals.Trigger(_graphChangedSignal);
         }
     }
 }
