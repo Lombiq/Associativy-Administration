18fc50973b340a89dbe527231ef20471ea042111
Piedone <piedone@pyrocenter.hu>
2012-08-16 01:34:12 +0200

    Making AssociativyGraphPart.AllContentTypes lazy-loading better

--- Handlers/AssociativyGraphPartHandler.cs
+++ Handlers/AssociativyGraphPartHandler.cs
@@ -5,6 +5,7 @@ using Orchard.ContentManagement.Handlers;
 using Orchard.ContentManagement.MetaData;
 using Orchard.Data;
 using Orchard.Environment.Extensions;
+using Orchard.Environment;
 
 namespace Associativy.Administration
 {
@@ -13,19 +14,19 @@ namespace Associativy.Administration
     {
         public AssociativyGrapParthHandler(
             IRepository<AssociativyGraphPartRecord> repository,
-            IContentDefinitionManager contentDefinitionManager)
+            Work<IContentDefinitionManager> contentDefinitionManagerWork)
         {
             Filters.Add(StorageFilter.For(repository));
 
-            OnInitializing<AssociativyGraphPart>((context, part) =>
-            {
-                part.AllContentTypesField.Loader(() =>
-                    {
-                        var containedTypes = part.ContainedContentTypes;
-                        var types = contentDefinitionManager.ListTypeDefinitions();
-                        return types.Select(type => new ContentType { Name = type.Name, IsContained = containedTypes.Contains(type.Name) }).ToList();
-                    });
-            });
+            OnActivated<AssociativyGraphPart>((context, part) =>
+                {
+                    part.AllContentTypesField.Loader(() =>
+                        {
+                            var containedTypes = part.ContainedContentTypes;
+                            var types = contentDefinitionManagerWork.Value.ListTypeDefinitions();
+                            return types.Select(type => new ContentType { Name = type.Name, IsContained = containedTypes.Contains(type.Name) }).ToList();
+                        });
+                });
         }
 
         protected override void GetItemMetadata(GetContentItemMetadataContext context)
