d21a771358dcd295136a661de74f86b2a147d2d3
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2013-01-31 23:55:12 +0100

    Migration for ad-hoc graphs

--- Migrations/AdhocGraphMigrations.cs
+++ Migrations/AdhocGraphMigrations.cs
@@ -1,15 +1,32 @@
 ﻿using Associativy.Administration.Models;
 using Associativy.Extensions;
+using Orchard.ContentManagement;
 using Orchard.ContentManagement.MetaData;
 using Orchard.Core.Contents.Extensions;
+using Orchard.Data;
 using Orchard.Data.Migration;
+using Orchard.Environment;
 using Orchard.Environment.Extensions;
+using System.Linq;
 
 namespace Associativy.Administration.Migrations
 {
     [OrchardFeature("Associativy.Administration.AdhocGraphs")]
     public class AdhocGraphMigrations : DataMigrationImpl
     {
+        private readonly Work<IRepository<AdhocGraphNodeConnector>> _connectorRepository;
+        private readonly Work<IContentManager> _contentManagerWork;
+
+
+        public AdhocGraphMigrations(
+            Work<IRepository<AdhocGraphNodeConnector>> connectorRepository,
+            Work<IContentManager> contentManagerWork)
+        {
+            _connectorRepository = connectorRepository;
+            _contentManagerWork = contentManagerWork;
+        }
+
+
         public int Create()
         {
             SchemaBuilder.CreateTable(typeof(AdhocGraphNodeConnector).Name,
@@ -52,6 +69,19 @@ namespace Associativy.Administration.Migrations
                     table.DropIndex("Connection");
                     table.CreateIndex("Connection", "GraphName", "Node1Id", "Node2Id");
                 });
+
+            #region Existing graphs migration
+            var graphs = _contentManagerWork.Value.Query("AssociativyGraph").List();
+            if (graphs.Any())
+            {
+                // Dealing with only one ad-hoc graph for now
+                var graphName = graphs.First().As<AssociativyGraphPart>().GraphName;
+                foreach (var connection in _connectorRepository.Value.Table)
+                {
+                    connection.GraphName = graphName;
+                }
+            } 
+            #endregion
 
 
             return 2;
